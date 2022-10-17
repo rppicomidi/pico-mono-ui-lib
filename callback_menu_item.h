a6e8b2b09219c6cdb38b22055c5791e5cca8436a
rppicomidi <rppicomidi@gmail.com>
2022-10-17 10:54:32 -0700

    Allow Callback_menu_item to return a result other than no_op

--- callback_menu_item.h
+++ callback_menu_item.h
@@ -35,12 +35,14 @@ namespace rppicomidi
 class Callback_menu_item : public Menu_item
 {
 public:
-    Callback_menu_item(const char* text_, Mono_graphics& screen_, const Mono_mono_font& font_, View* callback_view_, void (*callback_)(View*))  :
-        Menu_item{text_, screen_, font_}, callback_view{callback_view_}, callback{callback_} {}
+    Callback_menu_item(const char* text_, Mono_graphics& screen_, const Mono_mono_font& font_, View* callback_view_, void (*callback_)(View*),
+    View::Select_result select_action_=View::Select_result::no_op)  :
+        Menu_item{text_, screen_, font_}, callback_view{callback_view_}, callback{callback_}, select_action{select_action_} {}
 
-    virtual View::Select_result on_select(View**) { callback(callback_view); return View::Select_result::no_op; }
+    virtual View::Select_result on_select(View**) { callback(callback_view); return select_action; }
 protected:
     View* callback_view;
     void (*callback)(View* callback_view_);
+    View::Select_result select_action;
 };
 }
